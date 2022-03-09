fe7fae1f6eb75af78088b18f3041b77a0dde39f4
liq19ch <s4031630087@gmail.com>
2022-03-10 00:16:33 +0800

    feature: studentController updated.

--- src/com/school/controller/StudentController.java
+++ src/com/school/controller/StudentController.java
@@ -1,15 +1,26 @@
 package com.school.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.school.dao.StudentDAO;
+import com.school.entity.Student;
+
 @Controller
 @RequestMapping("/student")
 public class StudentController {
+	
+	@Autowired
+	private StudentDAO studentDAO; 
 
 	@RequestMapping("/list")
 	public String listStudents(Model model) {
+		List<Student> students = studentDAO.getStudents();
+		model.addAttribute("students", students);
 		return "list-students";
 	}
 }
