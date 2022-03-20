2320d6670b5170b3de75576b3f17e5493703978f
liq19ch <s4031630087@gmail.com>
2022-03-20 22:36:10 +0800

    feature: securityconfig & appinitializer added

---
+++ src/main/java/com/school/config/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.school.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		UserBuilder usersBuilder = User.withDefaultPasswordEncoder();
+		auth.inMemoryAuthentication().withUser(usersBuilder.username("student").password("test123").roles("STUDENT"))
+		.withUser(usersBuilder.username("teacher").password("test123").roles("TEACHER"))
+		.withUser(usersBuilder.username("admin").password("test123").roles("ADMIN"));
+
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().anyRequest().authenticated().and()
+		.formLogin().loginPage("/loginPage").loginProcessingUrl("/authenticateTheUser").permitAll()
+		.and().logout().permitAll();
+	}
+	
+	
+
+}
